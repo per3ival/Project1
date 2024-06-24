8e23e354c8c4e7757e41679fa32bf19e5cef3857
DONG HYUN KIM <per3ival@gmail.com>
2024-06-24 15:36:53 +0900

    빼기 추가

--- main.py
+++ main.py
@@ -7,3 +7,17 @@ num1, num2, res = 100, 200, 0
 res = add_func(num1, num2)
 print(num1, "+", num2, "=", res)
 
+## 함수선언부
+def add_func(n1, n2):
+ returnn1+n2
+ 
+def sub_func(n1, n2):
+ returnn1-n2
+ ## 전역변수부
+num1, num2, res= 100, 200, 0
+
+ ## 메인코드부
+res= add_func(num1, num2)
+print(num1, "+", num2, "=", res)
+res= sub_func(num1, num2)
+print(num1, "-", num2, "=", res)
